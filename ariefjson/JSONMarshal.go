6c9b4681c2b6258229091e096b82ae226f195d00
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2022-05-03 16:35:09 +0700

    marshal idnent

--- ariefjson/JSONMarshal.go
+++ ariefjson/JSONMarshal.go
@@ -9,6 +9,15 @@ func Marshal(input any) (ret string) {
 	bf := bytes.NewBuffer([]byte{})
 	jsonEncoder := json.NewEncoder(bf)
 	jsonEncoder.SetEscapeHTML(false)
+	jsonEncoder.Encode(input)
+	ret = bf.String()
+	return
+}
+
+func MarshalIndent(input any) (ret string) {
+	bf := bytes.NewBuffer([]byte{})
+	jsonEncoder := json.NewEncoder(bf)
+	jsonEncoder.SetEscapeHTML(false)
 	jsonEncoder.SetIndent("", " ")
 	jsonEncoder.Encode(input)
 	ret = bf.String()
--- helper.go
+++ helper.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+
+	"gitlab.com/ariefhidayatulloh/easy-ig/ariefjson"
 )
 
 func JSONView(w http.ResponseWriter, r *http.Request, data interface{}, statuscode int) {
-	//log.Println(data)
-	view, _ := json.Marshal(data)
+	view := ariefjson.MarshalIndent(data)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statuscode)
 
-	w.Write(view)
+	w.Write([]byte(view))
 }
 
 func Log(data interface{}) {
